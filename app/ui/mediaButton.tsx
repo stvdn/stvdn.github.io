a54be57f2bfb99e5f47288d339617b2d338d39bf
stvdn <stevendanny2000@gmail.com>
2024-09-16 19:01:34 -0500

    add open link in other tab

--- app/ui/mediaButton.tsx
+++ app/ui/mediaButton.tsx
@@ -9,6 +9,7 @@ export const MediaButton: React.FC<MediaButtonProps> = ({ icon, link }) => {
   return (
     <a
       href={link}
+      target="_blank"
       className="p-2 flex items-center justify-center text-3xl text-blue-500 rounded-full bg-blue-500/10 border-2 border-blue-500/75 hover:text-neutral-50 hover:bg-blue-500/75 hover:border-blue-500"
     >
       <span className={icon}></span>
