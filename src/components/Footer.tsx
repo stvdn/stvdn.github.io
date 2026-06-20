e037ed614c9db0264a4ea784083deda86ed6e0a5
stvdn <stevendanny2000@gmail.com>
2026-06-20 09:08:07 -0500

    feat(footer): wrap in mouse follower

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,14 @@
+import { MouseFollower } from "./MouseFollower";
+
 export function Footer() {
   return (
-    <footer className="text-right mt-80 pb-8">
+    <MouseFollower>
+    <footer className="text-right pt-80 pb-8">
       <h2 className="group relative inline-block ml-auto text-lg font-bold text-white md:text-xl lg:text-2xl">
         Contact me
         <span className="absolute -bottom-0.5 left-1/2 h-0.5 w-0 bg-white transition-all duration-300 group-hover:left-0 group-hover:w-full" />
       </h2>
     </footer>
+    </MouseFollower>
   );
 }
